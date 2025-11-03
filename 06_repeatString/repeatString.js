dac1aa1ac94cdcf965260a0a3efa49823cba96aa
Nick Bourgeois <77581387+Nick676191@users.noreply.github.com>
2025-11-02 23:14:29 -0600

    Write in an error message for if a negative value is input into the function

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,8 @@
 const repeatString = function(str, repeats) {
     let strArr = [];
+    if (repeats < 0) {
+        return 'ERROR'
+    }
     for (let i = 0; i < repeats; i++) {
         strArr.push(str)
     }
