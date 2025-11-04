604ab43bc9a65a5af656378cf9628ea6b2294563
Nick Bourgeois <77581387+Nick676191@users.noreply.github.com>
2025-11-04 17:39:55 -0600

    write the functions to convert between the different temps

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fHeight) {
+  let celsius = (fHeight - 32) * (5/9);
+  // rounding the float value to 1 decimal place (would multiply and divide by 100 to get two decimal places)
+  return Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fHeight = (celsius * (9/5)) + 32;
+  // rounding the float value to 1 decimal place
+  return Math.round(fHeight * 10) / 10;
 };
 
 // Do not edit below this line
