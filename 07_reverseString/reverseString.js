69ea1b63a3d540d2250f13e29076621584901183
Nick Bourgeois <77581387+Nick676191@users.noreply.github.com>
2025-11-03 18:30:23 -0600

    Loop through the string from the end to the start and combine all parts of the string to reverse it

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reverseStr = ''
+    for (let i = str.length - 1; i >= 0; i--) {
+        let lastLetter = str[i];
+        reverseStr = reverseStr + lastLetter;
+    };
+    return reverseStr;
 };
 
 // Do not edit below this line
