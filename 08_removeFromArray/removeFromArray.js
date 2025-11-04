6ef533793d63e96b7b0cb62e8facd9b153cfe8d2
Nick Bourgeois <77581387+Nick676191@users.noreply.github.com>
2025-11-03 19:24:47 -0600

    Remove any amount of elements from an array with the rest parameter coded into the function call

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+// the rest parameter `...` in the function call allows us to accept any number of additional arguments to the function
+const removeFromArray = function(arr, ...elements) {
+    for (let element of elements) {
+        elementIndex = arr.indexOf(element);
+        // go through the loop if the element is not in the array
+        if (elementIndex === -1) {
+            continue;
+        } else {
+            arr.splice(elementIndex, 1);
+        };
+    };
+    return arr
 };
 
 // Do not edit below this line
