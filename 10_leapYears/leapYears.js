4d4002c7e40afc0c68223f3cdba217d0cbb7f036
Nick Bourgeois <77581387+Nick676191@users.noreply.github.com>
2025-11-04 17:23:57 -0600

    Write logic to find leap years by filtering out all of the false leap year values

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // leap years are divisible by 4, but not divisible by 100 (unless divisible by 400)
+    if (year % 4 !== 0) return false;
+    if (year % 100 === 0 && year % 400 !== 0) {
+        return false;
+    } else {
+        return true;
+    };
 };
 
 // Do not edit below this line
