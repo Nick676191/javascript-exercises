27a505b54041a3dcca3b3bf4ec4e33fd8acc60be
Nick Bourgeois <77581387+Nick676191@users.noreply.github.com>
2025-11-03 22:36:12 -0600

    Checked for any errors to the function would occur, sorted the arguments, then sum over the range of the two arguments given

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(...args) {
+    // const numArray = [firstInt, secondInt];
+    if (args.some(value => value < 0) || 
+        !(args.every(value => Number(value) === value)) ||
+        args.some(value => value % 1 !== 0)) {
+        return "ERROR";
+    };
+    // sort arguments in ascending order
+    let sortedNumArray = args.sort(
+        (a, b) => a - b
+    );
+    let theSum = 0;
+    for (let i = sortedNumArray[0]; i <= sortedNumArray[1]; i++) {
+        theSum += i
+    };
+    return theSum
 };
 
 // Do not edit below this line
