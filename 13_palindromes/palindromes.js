d03075567d0511f87c8c5a71ddf219b51d415bea
Nick Bourgeois <77581387+Nick676191@users.noreply.github.com>
2025-11-19 21:24:31 -0600

    Passed all of the tests

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,6 @@
 const palindromes = function (str) {
     // just need to get rid of any special characters then reverse the string
+    // the regex code `/\W/g` gets rid of any characters that aren't part of the alphabet
     let newStr = str.replace(/\W/g, '').toLowerCase();
     let strArr = newStr.split("");
     let revStr = strArr.reverse().join("");
