962e97e644691e7a9e850d9c41b0e8b9ea31ef14
Nick Bourgeois <77581387+Nick676191@users.noreply.github.com>
2025-11-19 21:57:48 -0600

    Setting up to test the code with an iterative attempt at writing the function

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,14 +1,35 @@
-// recursive example 
+// // recursive example 
+// const fibonacci = function(num) {
+//     let numb = Number(num);
+//     if (numb < 0) {
+//         return "OOPS";
+//     } else if (numb === 0) {
+//         return 0;
+//     } else if (numb === 1) {
+//         return 1;
+//     } else {
+//         return fibonacci(numb - 1) + fibonacci(numb - 2);
+//     };
+// };
+
+// iterative example
 const fibonacci = function(num) {
     let numb = Number(num);
     if (numb < 0) {
-        return "OOPS"
+        return "OOPS";
     } else if (numb === 0) {
-        return 0
+        return 0;
     } else if (numb === 1) {
-        return 1
+        return 1;
     } else {
-        return fibonacci(numb - 1) + fibonacci(numb - 2);
+        let iterOne = 0;
+        let iterTwo = 1;
+        for (let i = 2; i <= numb; i++) {
+            let finAns = iterOne + iterTwo;
+            iterOne = iterTwo;
+            iterTwo = finAns;
+        };
+        return finAns;
     };
 };
 
