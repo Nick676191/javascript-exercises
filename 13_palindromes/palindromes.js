f85fbc9577230708ce84a6102c1b174cc950fce9
Nick Bourgeois <77581387+Nick676191@users.noreply.github.com>
2025-11-19 21:20:11 -0600

    Initial attempt at the exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    // just need to get rid of any special characters then reverse the string
+    let newStr = str.replace(/\W/g, '').toLowerCase();
+    let strArr = newStr.split("");
+    let revStr = strArr.reverse().join("");
+    return newStr === revStr;
 };
 
 // Do not edit below this line
