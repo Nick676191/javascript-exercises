5faaf335b43d13d97bdbcaa92c329b7a5b1c0df2
Nick Bourgeois <77581387+Nick676191@users.noreply.github.com>
2025-11-20 19:01:53 -0600

    Initial attempt at writing the function

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(objArr) {
+    objArr.map(person => {
+        if (person.yearOfDeath === undefined) {
+            person.yearOfDeath = new Date().getFullYear;
+        };
+    });
+    objArr.sort((a, b) => {
+        a = a.yearOfDeath - a.yearOfBirth;
+        b = b.yearOfDeath - b.yearOfBirth;
+    });
+    return objArr[0].name;
 };
 
 // Do not edit below this line
