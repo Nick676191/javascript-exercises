7d183d178cb323c0bf4d0daf4c319c375e1a59eb
Nick Bourgeois <77581387+Nick676191@users.noreply.github.com>
2025-11-20 23:12:48 -0600

    Passed all of the tests

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
 const findTheOldest = function(objArr) {
-    let sortArr = objArr.sort((a, b) => {
+    // use of ternary operator to set `yearOfDeath` key to a new value or its current value according to wheher it is defined
+    let newArr = objArr.map(obj => {
+        // `hasOwnProperty` returns a boolean after checking if the object has that key in it
+        obj.yearOfDeath = obj.hasOwnProperty("yearOfDeath") 
+        ? obj.yearOfDeath 
+        : new Date().getFullYear()
+        // make sure to return the value in the array to avoid returning undefined
+        return obj;
+    });
+    console.log(newArr);
+    let sortArr = newArr.sort((a, b) => {
+        // define the values being sorted
         const aLife = a.yearOfDeath - a.yearOfBirth;
         const bLife = b.yearOfDeath - b.yearOfBirth;
         return bLife - aLife;
